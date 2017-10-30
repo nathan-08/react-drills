3c2b60686477ec20061d30bf20fc647498b35c25
Nathan Klundt <nathanryan001@gmail.com>
2017-10-30 14:30:14 -0600

    app1 solved

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      userInput: '',
+      array: []
+    }
+  this.handleChange = this.handleChange.bind(this)
+  this.handleKeyPress = this.handleKeyPress.bind(this)
+}
+  handleChange(e){
+    this.setState({ userInput: e.target.value })
+  }
+  handleKeyPress(e){
+    if(e.key == 'Enter'){
+      this.setState({
+        userInput: '',
+        array: [...this.state.array, this.state.userInput]
+      })
+    }
+  }  
+
+  
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input placeholder="type here" value={this.state.userInput} 
+        onChange={this.handleChange}
+        onKeyPress={this.handleKeyPress}/>
+        <div>
+        <ul>{this.state.array.map(e=> <li>{e}</li> )}</ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
